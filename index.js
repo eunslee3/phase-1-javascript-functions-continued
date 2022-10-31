5eca7d452d65154381cdef968c5c32afcf70826d
Samuel <lee.eunseong7856@gmail.com>
2022-10-30 19:22:15 -0500

    commit tree bad8543dab520108a16aa16ef4d797c719170e7e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(work = "This Monday, I will go to the office.") {
+    if(work === "work from home") {
+        return "This Monday, I will work from home."
+    }
+    else {
+        return work;
+    }
+}
+
+function wrapAdjective (wrap = "*") {
+    return function (adjective = "special") {
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+    //`${wrap}${adjective}${wrap}!`
+}
+let result = wrapAdjective("*");
